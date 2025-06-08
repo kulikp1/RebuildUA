7c4a81d38965da44d6b94067b1f42ba43b6b59cc
kulikp1 <pashakylik2@gmail.com>
2025-06-08 03:42:40 +0300

    add header to pages

--- src/components/CompanyPage/CompanyPage.jsx
+++ src/components/CompanyPage/CompanyPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./CompanyPage.module.css";
+import Header from "../Header/Header";
 
 const CompanyPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Ласкаво просимо, Компаніє!</h1>
-      <p className={styles.subtitle}>
-        Це ваша компанійська панель платформи REBUILD.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Ласкаво просимо, Компаніє!</h1>
+        <p className={styles.subtitle}>
+          Це ваша компанійська панель платформи REBUILD.
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./UserPage.module.css";
+import Header from "../Header/Header";
 
 const UserPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Ласкаво просимо, Громадянине!</h1>
-      <p className={styles.subtitle}>
-        Це ваша особиста сторінка на платформі REBUILD.
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Ласкаво просимо, Громадянине!</h1>
+        <p className={styles.subtitle}>
+          Це ваша особиста сторінка на платформі REBUILD.
+        </p>
+      </div>
+    </>
   );
 };
 
